45960ca5e3c0fa15354e7442bfc4f0034dee7c1b
QuangVD2 <quangvd2@gmail.com>
2021-05-12 00:18:34 +0700

    init project

---
+++ Base/BaseScreen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyCoin.Base
+{
+    public abstract class BaseScreen : Form
+    {
+        protected BaseScreen(): base() { }
+        protected void InitBase()
+        {
+            this.Controls.OfType<Button>().ToList().ForEach((control) =>
+            {
+                control.Click += new System.EventHandler(this.OnTouchEnded);
+            });
+        }
+
+        public virtual void OnTouchEnded(object sender, EventArgs e) { }
+
+        public abstract void ShowGui();
+
+        public abstract void HideGui();
+    }
+}
